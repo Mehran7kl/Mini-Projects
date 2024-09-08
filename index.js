841c69fd116898b4b65b00217edeb24c87bd98c0
Mehran7kl <raisimehran7@gmail.com>
2024-09-08 14:17:17 +0330

    added support dor tags

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 
+const urlParams = (()=>{
+    try{
+        return new URLSearchParams(window.location.search)
+    }catch{ return null}})();
+
 function getPath(file)
 {
-    const base = window.location.href.replace("/index.html","/");
+    const i = window.location.pathname.indexOf(myproject.base) + myproject.base.length;    
+    const base = window.location.pathname.slice(0,i);
     return base+file;
 }
-function putLink(file,title,description){
+
+function putLink(file,title,description,tags){
     
     const con = document.querySelector("#code-links")
     con.innerHTML+=`
     <article class="code-link">
         <h3><a href="${getPath(file)}">${title}</a></h3>
         <h4>${description}</h4>
+        <div class="code-tags">
+            ${tags.map(tag=>`<button class="code-tag" onclick="location.href+='?tags=${tag}'" >${tag}</button>`).join(" ")}
+        </div>
     </article>
     `
 }
 
-htmlFiles.forEach(file=>{
+myproject.htmlFiles.forEach(file=>{
     const doc = document.createElement("html")
     fetch(`${getPath(file)}`)
     .then(res=>res.text())
@@ -23,8 +33,10 @@ htmlFiles.forEach(file=>{
         doc.innerHTML=text;
         const title = doc.querySelector('title').innerText ;
         const description = doc.querySelector('[name="description"]')?.getAttribute("content") ;
-        if(description){
-            putLink(file,title,description);
+        const tags = doc.querySelector('[name="keywords"]')?.getAttribute("content").split(",").map(s=>s.trim());
+        const urlTags = urlParams?.get("tags");
+        if(tags&& title&&description){
+            if(urlTags == null || tags.includes(urlTags) ) putLink(file,title,description,tags);
         }
     })
     
